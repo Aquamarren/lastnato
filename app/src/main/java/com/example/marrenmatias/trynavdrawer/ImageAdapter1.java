686b88975100bb9eb9a1a512399254418e51454a
MarrenMatias <marsreyesmatias@gmail.com>
2017-03-12 10:22:17 +0800

    firstcommit

---
+++ app/src/main/java/com/example/marrenmatias/trynavdrawer/ImageAdapter1.java
@@ -0,0 +1,8 @@
+package com.example.marrenmatias.trynavdrawer;
+
+/**
+ * Created by MarrenMatias on 3/9/2017.
+ */
+
+public class ImageAdapter1 {
+}
---
+++ app/src/main/java/com/example/marrenmatias/trynavdrawer/ViewSettings.java
@@ -0,0 +1,13 @@
+package com.example.marrenmatias.trynavdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_settings);
+    }
+}
